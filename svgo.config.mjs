57172a5bd55fe1a41f68f6fa4d98739b77e052b9
Kristian Jensen <kristianjensen5@gmail.com>
2025-10-31 13:22:28 +0100

    Swap to new map graphic and normalise trigger IDs

--- svgo.config.mjs
+++ svgo.config.mjs
@@ -1,6 +1,28 @@
+import path from 'node:path';
+
 export default {
   js2svg: {
     pretty: true,
     indent: 2
-  }
+  },
+  plugins: [
+    'preset-default',
+    {
+      name: 'prefixIds',
+      params: {
+        delim: '-',
+        prefix: (_node, info) => {
+          if (info.path) {
+            const base = path.basename(info.path, path.extname(info.path));
+            const normalized = base.replace(/[^a-zA-Z0-9_-]/g, '').toLowerCase();
+            if (normalized.length > 0) {
+              return normalized;
+            }
+          }
+          return 'svg';
+        },
+        prefixClassNames: true
+      }
+    }
+  ]
 };
